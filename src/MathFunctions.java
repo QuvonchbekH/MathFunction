4afbd37e3ade5b2a6c37ab008a5e52bb4effcac6
Quvonchbek <quvonchbek20052403@gmail.com>
2023-12-01 20:02:59 +0500

    first commit

--- src/MathFunctions.java
+++ src/MathFunctions.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class MathFunctions {
-    static double PI;
-    static double e;
+    static double PI=3.14;
+    static double e=2.7;
+    public int number = 1_000_000;
+    static{
+        double S = doira(3);
+        double Pow = MathPow(5);
+        if (S>Pow){
+            System.out.println("Yuza");
+        }
+        else System.out.println("Daraja");
+    }
 
     static double doira(int r){
         double S = PI* Math.pow(r, 2);
@@ -15,4 +26,15 @@ public class MathFunctions {
         float S = (float)(Math.sqrt(p*(p-a)*(p-b)*(p-c)));
         return S;
     }
-}
+
+    public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+            MathFunctions mathFunctions = new MathFunctions();
+//            MathFunctions.PI = input.nextDouble();
+//            MathFunctions.e = input.nextDouble();
+
+            System.out.println(doira(10));
+            System.out.println(MathPow(6));
+            System.out.println(Shape(3, 4, 5));
+        }
+    }
